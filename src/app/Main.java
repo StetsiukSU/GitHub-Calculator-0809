fa407b1f5c0c08343d5c939eaff4ef375d019dcc
sstetsiuk <sstetsiuk@itsmartflex.com>
2022-09-23 23:55:57 +0300

    Add

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,6 +11,10 @@ public class Main {
         System.out.println(c);
         int d = multiply(a, b);
         System.out.println(d);
+        int e = minus(a, b);
+        System.out.println(e);
+        int f = divide(a, b);
+        System.out.println(f);
     }
 
     private static int add(int a, int b) {
@@ -20,4 +24,13 @@ public class Main {
     private static int multiply(int a, int b) {
         return a * b;
     }
+
+    private static int minus(int a, int b) {
+        return a - b;
+    }
+
+    private static int divide(int a, int b) {
+        return a / b;
+    }
+
 }
